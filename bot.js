f818d953abf3482aa4cac001712cd6a5c5a14f48
Zianii <41494410+Zianii@users.noreply.github.com>
2019-02-04 22:00:27 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -316,7 +316,7 @@ client.on('message', message => {
     if (message.content === '-mhelp') {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**أوامر الميوزك...**')
-        .setDescription('**برفكس البوت (%)**')
+        .setDescription('**برفكس البوت (-)**')
         .addField('play', 'لتشغيل اغنية')
         .addField('join', 'دخول رومك الصوتي')
         .addField('stop', 'الخروج من رومك الصوتي')
